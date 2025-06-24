0c00d9a0125b593cc254a78beee657ce2fa2f5b2
Gaurav Agarwalla <agarwalgaurav1809@gmail.com>
2025-06-25 01:21:19 +0530

    final changes

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
                     <form className="d-flex" role="search">
                         <ul className="navbar-nav mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href={`${process.env.REACT_APP_DASHBOARD_URL}`}>
+                                <a className="nav-link active" aria-current="page" href="https://tradexa-dashboard-main.onrender.com">
                                     Register
                                 </a>
                             </li>
